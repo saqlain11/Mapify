cd4fb00d406ce2ec25430d06537330124a80ad6e
SaqAsh <saqlain.ashraf@medznmore.com>
2025-05-08 20:24:49 +0800

    initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          // This needs to be mirrored in tsconfig.json
+          '@Mapify': './src',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/view/map.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from 'react-native';
+
+const Map = () => {
+  return (
+    <SafeAreaView>
+        <Text >here are i am to</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Map;
